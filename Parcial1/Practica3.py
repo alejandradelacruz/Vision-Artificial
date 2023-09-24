ce6f460097c98c244d95b02d3a55c0b15992eeef
Alejandra De la cruz <40179462+alejandradelacruz@users.noreply.github.com>
2023-09-23 19:54:27 -0600

    Update Practica3.py
    actualizadp

--- Parcial1/Practica3.py
+++ Parcial1/Practica3.py
@@ -2,40 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-def perceptron_activation(x, w):
+def perceptron(x, w):
     return np.dot(x, w)
 
 X = np.array([[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1],
               [1, 0, 0], [1, 0, 1], [1, 1, 0], [1, 1, 1]])
-y = np.array([0, 1, 1, 1, 1, 1, 1, 1])
+y = np.array([1, 1, 1, 1, 0, 0, 0, 0])  
 
 w = np.zeros(4)  
-for i in range(4):
+for i in range(3):
     w[i] = float(input(f'Ingrese el valor del peso w{i + 1}: '))
 
-r = float(input('Ingrese el valor del factor de correlación (r): '))
+w[3] = float(input('Ingrese el valor del peso w0: '))
+
+#Factor de correlacion 
+r = float(input('Ingrese el valor de r: '))
 
 converge = False
 etapa = 0
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+figura = plt.figure()
+axis = figura.add_subplot(111, projection='3d')
 
 while not converge:
     converge = True
     print(f'Étapa {etapa + 1}:')
     
     for i, x in enumerate(X):
-        x_extended = np.append(x, 1)  #
-        activation = perceptron_activation(x_extended, w)
+        xn = np.append(x, 1)  
+        fsal = perceptron(xn, w)
         
-        if activation >= 0 and y[i] == 0:
+        if fsal >= 0 and y[i] == 0:
+            #Formula para clase 1: XnTw >= 0 pertenece a la clase 1
             print(f'Para entrada {x}, w pertenece a la clase 1')
-            w = w - r * x_extended
+            #Formula para corregir clase 1: wn+1 = wn - r * xn
+            w = w - r * xn
             converge = False
-        elif activation <= 0 and y[i] == 1:
+        elif fsal <= 0 and y[i] == 1:
+            #Formula para clase 2: XnTw <= 0 pertenece a la clase 2
             print(f'Para entrada {x}, w pertenece a la clase 2')
-            w = w + r * x_extended
+            #Formula para corregir clase 2: wn+1 = wn + r * xn
+            w = w + r * xn
             converge = False
         else:
             print(f'Para entrada {x}, w se mantiene sin cambios')
@@ -45,19 +52,26 @@ while not converge:
 print('\nEl perceptrón ha convergido:')
 print(f'Valores finales de los pesos: {w}')
 
-# Graficar el espacio de clases final en 3D
 for i, x in enumerate(X):
     if y[i] == 0:
-        ax.scatter(x[0], x[1], x[2], c='r', marker='o', label='Clase 1')
+        axis.scatter(x[0], x[1], x[2], c='c', marker='o', label='Clase 1')
     else:
-        ax.scatter(x[0], x[1], x[2], c='b', marker='x', label='Clase 2')
+        axis.scatter(x[0], x[1], x[2], c='m', marker='x', label='Clase 2')
 
 xx, yy = np.meshgrid(np.linspace(0, 1, 2), np.linspace(0, 1, 2))
 zz = (-w[0] * xx - w[1] * yy - w[3]) / w[2]
-ax.plot_surface(xx, yy, zz, alpha=0.5)
+axis.plot_surface(xx, yy, zz, alpha=0.5)
 
-ax.set_xlabel('X')
-ax.set_ylabel('Y')
-ax.set_zlabel('Z')
+axis.set_xlabel('X')
+axis.set_ylabel('Y')
+axis.set_zlabel('Z')
 
+axis.text(0, 0, 0, 'Clase 1', color='red')
+axis.text(1, 1, 1, 'Clase 1', color='red')
+axis.text(0, 1, 0, 'Clase 1', color='red')
+axis.text(0, 0, 1, 'Clase 1', color='red')
+axis.text(1, 0, 0, 'Clase 2', color='blue')
+axis.text(1, 0, 1, 'Clase 2', color='blue')
+axis.text(0, 1, 1, 'Clase 2', color='blue')
+axis.text(1, 1, 0, 'Clase 2', color='blue')
 plt.show()
